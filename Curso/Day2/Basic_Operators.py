02834c5144a2c47db2f58bad40dfecbdae14845b
erick <kire.erickoz@gmail.com>
2025-02-24 17:56:08 -0600

    day 2

---
+++ Curso/Day2/Basic_Operators.py
@@ -0,0 +1,18 @@
+print("My age: " + str(12))
+print(100 + 20)
+print(100 - 20)
+print(100 * 20)
+print(100 / 20)
+print(100 // 20) #Se convierte el resultado en tipo int e elimina las decimas
+print(100 % 20)
+print(4 ** 2) #Potencias cuatro a la segunda potencia
+
+#Las operaciones tienen una prioridad:
+#1 - parentesis
+#2 - Expresions
+#3 - Multiplicacion
+#4 - Divicion
+#5 - Suma 
+#6 - Resta
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
---
+++ Curso/Day2/DataTypes.py
@@ -0,0 +1,14 @@
+print("Hello"[0])
+
+print("Hello"[-2])
+
+#colocar guines bajos para entender mejor el numero en lugar de comas
+print(123_456_789 + 1)
+
+#obtener el tipo de dato
+print(type("Hello"))
+
+#CAsteo
+print(int("123") + int("456"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
